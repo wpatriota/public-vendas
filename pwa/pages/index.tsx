0f15d47eea789c9508b3f21711cb1213dbeb17d1
Wellington Patriota <w_patriota@hotmail.com>
2022-10-09 01:39:36 -0300

    bundle

--- pwa/pages/index.tsx
+++ pwa/pages/index.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Welcome = () => (
   <>
     <Head>
-      <title>Welcome to API Platform!</title>
+      <title>Sistema - Doações</title>
     </Head>
 
     <div className="welcome">
